519cc2798463cf037e9ab27f703a59f490f3d594
Usman <usys537@yahoo.com>
2013-06-11 22:53:45 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gombites::Application.config.secret_token = '2e9aba11d86253524eebdbc38025c381c54c500dc409119ce33b496dc21e9112fcf5581b9d7613c4b252e5d1966c96c9273a092fb50335692b5da151a731424f'
